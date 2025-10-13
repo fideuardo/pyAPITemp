90a7e50854d3b29f1abbec2f4bb347e12c423b52
Fidel Cabañas <35480152+fideuardo@users.noreply.github.com>
2025-10-13 17:19:59 -0600

    Init repository (#1)

---
+++ main.py
@@ -0,0 +1,13 @@
+from PySide6.QtWidgets import QApplication
+from views.main_window import MainWindow
+import sys
+
+def main():
+    app = QApplication(sys.argv)
+    win = MainWindow()
+    win.resize(800, 600)
+    win.show()
+    sys.exit(app.exec())
+
+if __name__ == "__main__":
+    main()
