8a48e83deefd467da9f680b6d2a5de803d1f5765
fideuardo <fideuardo@gmail.com>
2025-10-20 17:34:21 -0600

    the file structure was updated

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from PySide6.QtWidgets import QApplication
+from src.TempSensor import TempSensor
 from views.main_window import MainWindow
 import sys
 
 def main():
+    
     app = QApplication(sys.argv)
     win = MainWindow()
     win.resize(800, 600)
@@ -10,4 +12,5 @@ def main():
     sys.exit(app.exec())
 
 if __name__ == "__main__":
+    sensor = TempSensor(auto_open=True)
     main()
